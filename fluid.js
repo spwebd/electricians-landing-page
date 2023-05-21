ed04300fe7eae49748f7354937d3571b71dfa676
spwebd <97314402+spwebd@users.noreply.github.com>
2023-05-21 16:17:35 +0100

    added some JS
    2 x alert boxes

---
+++ fluid.js
@@ -0,0 +1,7 @@
+document.getElementById("services").onmousemove=function(){
+    alert("If you are interested in are services, don't forget we have a special discount offer on at the moment");
+}
+
+document.getElementById("gallery").onclick=function(){
+    alert("Gallery is under design at the moment, please try again soon");
+}
