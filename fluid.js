975c48e26e4f4638688a022e368f5be9609ed241
spwebd <97314402+spwebd@users.noreply.github.com>
2023-05-31 16:31:12 +0100

    change

--- fluid.js
+++ fluid.js
@@ -5,3 +5,14 @@ document.getElementById("services").onmousemove=function(){
 document.getElementById("gallery").onclick=function(){
     alert("Gallery is under design at the moment, please try again soon");
 }
+
+function bod(){
+    alert("The page has loaded." + new Date());
+}
+
+
+
+
+document.getElementById("butt1").onclick=function(){
+  prompt("Leave your name and well get back to you", "name..." );
+}
